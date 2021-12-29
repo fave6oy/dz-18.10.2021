654ea0e95e3709f53139566e0170186ad21b94c3
Denis <91890468+fave6oy@users.noreply.github.com>
2021-12-29 05:29:42 +0300

    Create dz 18.10.2021.cpp

---
+++ dz 18.10.2021.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+//сиракузская последовательность
+int main()
+{
+	int n;
+	do
+	{
+		std::cout << "Input positive integer n: ";
+		std::cin >> n;
+	} while (n < 1);
+	int m = 0, cnt = 0;
+	do
+	{
+		++cnt;
+		if (n > m)
+		{
+			m = n;
+		}
+		if (n % 2 == 0)
+		{
+			n /= 2;
+		}
+		else
+		{
+			n = 3 * n + 1;
+		}
+	} while (n > 1);
+	std::cout << "Number of steps: " << cnt << "\nMax: " << m << std::endl;
+}
